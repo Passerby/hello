143107d4498ecc6d73eeb0150340748b5929c141
yune <zyune@qq.com>
2015-06-23 21:15:54 +0800

    test db

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem "twitter-bootstrap-rails"
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'pg'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ app/models/jobseeker.rb
@@ -0,0 +1,6 @@
+class Jobseeker < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
