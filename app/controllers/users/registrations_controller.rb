ac6f323e72cf9883492f98281a46e1b9c8cce9d1
yune <zyune@qq.com>
2015-06-27 16:31:54 +0800

    注册

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -36,11 +36,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  protected
-  def update_resource(resource, params)
-    resource.update_without_password(params)
-  end
-
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.for(:sign_up) << :attribute
