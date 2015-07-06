b882b9f6919f5ebdde06a563742788befe0aae58
yune <zyune@qq.com>
2015-07-06 22:37:08 +0800

    logined

--- app/controllers/logined_controller.rb
+++ app/controllers/logined_controller.rb
@@ -1,4 +1,17 @@
 class LoginedController < ApplicationController
   def user
+  	if user_signed_in?
+  		render json: {logined: true}
+  	else
+  		render json: {logined: false}
+  	end
+  end
+
+  def jobposter
+  	if jobposter_signed_in?
+  		render json: {logined: true}
+  	else
+  		render json: {logined: false}
+  	end
   end
 end
