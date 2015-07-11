9c0e96de0e16f26a794f1f3449276ad4d5bca341
Luke Du <dr397379567@gmail.com>
2015-07-11 16:50:40 +0800

    增加个人中心页面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :authenticate_jobposter!, except: [:save_favorite_job]
+  before_action :authenticate_jobposter!, except: [:save_favorite_job, :delete_favorite_job]
+  before_action :authenticate_user!, only: [:save_favorite_job, :delete_favorite_job]
   def index
   	 @users = User.all
   #	 binding.pry
@@ -14,6 +15,15 @@ class UsersController < ApplicationController
       render js: "alert('收藏成功')" 
     else
       render js: "alert('请先登录')"
+    end
+  end
+
+  def delete_favorite_job
+    favorite = FavoriteJob.find_by(user_id: current_user.id, job_id: params[:job_id])
+    if favorite.destroy
+      render js: "location.reload();"
+    else
+      render js: "alert('delete favorite job  fail!');"
     end
   end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,6 @@
 class Job < ActiveRecord::Base
+  attr_accessor :applications_count
+
   belongs_to :city, class_name: 'Admin::Setting::City', foreign_key: 'admin_setting_city_id'
   belongs_to :company
   belongs_to :jobposter
@@ -11,7 +13,5 @@ class Job < ActiveRecord::Base
   validates :requirement, presence: true
   validates :company_id, presence: true
   validates :admin_setting_city_id, presence: true
-
-  attr_accessor :applications_count
 
 end
